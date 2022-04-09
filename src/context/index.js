bead262b8debb1063014694a083f393132d7c7af
Andres Campo <andrescv103@gmail.com>
2022-04-09 11:15:25 +0200

    Signup form

--- src/context/index.js
+++ src/context/index.js
@@ -1,13 +1,27 @@
-import { createContext } from "react";
+
+import { createContext, useState } from "react";
+import { client } from "client";
 
 export const AuthContext = createContext();
 
 export function AuthContextProvider({ children }) {
-  const value = {
-    user: {
-      email: "a@a.com",
-    },
+  const [user, setUser] = useState(null);
+
+  const signup = async (firstName, lastName, email, password) => {
+    const response = await client.post("./auth/signup", {
+      firstName,
+      lastName,
+      email,
+      password,
+    });
+    console.log(response);
   };
 
-  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
-}
+  const login = async (email, password) => {};
+
+  const value = {
+    user,
+    signup,
+  };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;}
