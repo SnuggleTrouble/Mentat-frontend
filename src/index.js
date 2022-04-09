8ca7d8b01f4c6f0c3cf833910d34de519978b7f9
SnuggleTrouble <stabjo@outlook.com>
2022-04-09 13:44:55 +0200

    Boilerplate setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "../../frontend-boilerplate/src/App";
-import reportWebVitals from "../../frontend-boilerplate/src/reportWebVitals";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Landing, Home, NotFound } from "pages";
 import { PrivateRoute } from "components";
