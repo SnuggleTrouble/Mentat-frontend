9ccf66e072badf939f2f0e207f31a1f7a10fed87
Andres Campo <andrescv103@gmail.com>
2022-04-23 10:33:45 +0200

    Comment baseline

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./Post.module.css";
 import axios from "axios";
+import { Link } from "react-router-dom";
 import { Support } from "components/Support/Support";
 import { ListOfComments } from "components/ListOfComments";
 import { AddComment } from "components/AddComment";
@@ -24,6 +25,15 @@ export function Post({
   const [newPostTitle, setNewPostTitle] = useState(title);
   const [newPostCategory, setNewPostCategory] = useState(category);
   const [newPostContent, setNewPostContent] = useState(content);
+
+  const HandleShowComment() => {
+    return (
+      <button>
+        {user && <Link to="/post/:id">Comments</Link>}
+      </ button>
+    );
+  }
+  
 
   // handle Post deletion
   const handleDelete = () => {
