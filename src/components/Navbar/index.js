c26c21778e306c32a7bb4f91c61010ec231a3410
SnuggleTrouble <stabjo@outlook.com>
2022-05-03 20:12:14 +0200

    add categories to navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -8,9 +8,10 @@ export function Navbar() {
     <div className=" flex p-5 bg-emerald-500 text-emerald-900 font-semibold">
       <nav className=" flex flex-row justify-evenly gap-20">
         <div className=" text-emerald-800 ">
-          <img src="./Mentat-logo.png" width={110} alt=""/>
+          <img src="./Mentat-logo.png" width={110} alt="" />
           {!user && <Link to="/">Home</Link>}
         </div>
+        <div>{user && <Link to="/post/categories">Categories</Link>}</div>
         <div>{!user && <Link to="/landing">Home</Link>}</div>
         <div>
           {!user && <Link to="/login">Login</Link>}
